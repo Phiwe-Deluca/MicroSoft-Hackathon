0d113524a286cf2403f6a0ffaa92673af14ee5bd
momakgejhb024 <momakgejhb024@student.wethinkcode.co.za>
2025-05-03 15:22:13 +0200

    added files structure

--- neo_scan_backend/neo_scan_AI/neo_scan.js
+++ neo_scan_backend/neo_scan_AI/neo_scan.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import * as dotenv from "dotenv";
-import { AzureOpenAI } from "openai";
+import { AzureOpenAI } from "openai/index.mjs";
 
 dotenv.config();
 
